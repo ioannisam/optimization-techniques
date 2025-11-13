557936916b88d1b366223325f373f60ec40ccdd7
ioannis <i.michalainas@gmail.com>
2025-11-13 12:52:20 +0200

    implement dichotomous algorithm

---
+++ task1/src/dichotomous.m
@@ -0,0 +1,44 @@
+function [aK,bK,k,fevals,history] = dichotomous_method(f,a,b,l,e)
+   
+    if ~(isa(f,'function_handle'))
+        error('Input f must be a function handle.');
+    end
+    if ~(isscalar(a) && isscalar(b) && a < b)
+        error('Inputs a,b must be scalars with a < b.');
+    end
+    if ~(isscalar(l) && l > 0 && isscalar(e) && e > 0)
+        error('Inputs l and e must be positive scalars.');
+    end
+
+    k = 0;
+    fevals = 0;
+    history.a = a;
+    history.b = b;
+
+    while (b-a) > l
+        m = (a+b) / 2;
+        x1 = m - e;
+        x2 = m + e;
+        f1 = f(x1);
+        f2 = f(x2);
+        fevals = fevals + 2;
+
+        if f1 < f2
+            b = x2; % minimum in [a, x2]
+        else
+            a = x1; % minimum in [x1, b]
+        end
+
+        k = k + 1;
+        history.a(end+1) = a;
+        history.b(end+1) = b;
+
+        if k > 1e6
+            warning('Maximum number of iterations reached.');
+            break;
+        end
+    end
+
+    aK = a;
+    bK = b;
+end
