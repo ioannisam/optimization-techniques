99798156bb386cb20a9921c56e331e24758404b9
ioannis <i.michalainas@gmail.com>
2025-11-14 11:39:43 +0200

    complete task3

--- assignment1/src/fibonacci.m
+++ assignment1/src/fibonacci.m
@@ -10,19 +10,17 @@ function [aK,bK,k,fevals,history] = fibonacci(f,a,b,l)
         error('Input l must be a positive scalar.');
     end
 
-    F = [1, 1];  % fibonacci sequence
-    n = 2;
-    while F(n) < (b-a)/l
-        F(n+1) = F(n) + F(n-1);
-        n = n + 1;
+    F = [1 1];  % fibonacci sequence
+    while F(end) < (b-a)/l
+        F(end+1) = F(end) + F(end-1);
     end
+    n = length(F);
 
     x1 = a + (F(n-2)/F(n))*(b-a);
     x2 = a + (F(n-1)/F(n))*(b-a);
     f1 = f(x1);
     f2 = f(x2);
 
-    k = 0;
     fevals = 2;
     history.a = a;
     history.b = b;
@@ -32,18 +30,22 @@ function [aK,bK,k,fevals,history] = fibonacci(f,a,b,l)
             b = x2;
             x2 = x1;
             f2 = f1;
-            x1 = a + (F(n-k-2)/F(n-k))*(b-a);
-            f1 = f(x1);
+            
+            if n-k-2 >= 1
+                x1 = a + (F(n-k-2)/F(n-k))*(b-a);
+            else
+                x1 = a + 0.5*(b-a); % avoid F(0)
+            end
         else
             a = x1;
             x1 = x2;
             f1 = f2;
+
             x2 = a + (F(n-k-1)/F(n-k))*(b-a);
             f2 = f(x2);
         end
 
         fevals = fevals + 1;
-
         history.a(end+1) = a;
         history.b(end+1) = b;
     end
