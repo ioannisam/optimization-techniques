cc03f9107e401d429eb595024ed8597e8fa539c7
ioannis <i.michalainas@gmail.com>
2025-11-14 11:46:57 +0200

    complete task4

--- assignment1/objective_functions.m
+++ assignment1/objective_functions.m
@@ -7,6 +7,7 @@ df2 = @(x) 2*(x-1) + exp(x) - 5*cos(x+3);
 df3 = @(x) -3*exp(-3*x) - 2*(sin(x-2)-2).*cos(x-2);
 
 funcs = {f1, f2, f3};
+dfuncs = {df1, df2, df3};
 fnames = {'f1','f2','f3'};
 
 a0 = -1; b0 = 3;
--- assignment1/tasks/task4.m
+++ assignment1/tasks/task4.m
@@ -8,12 +8,12 @@ run(fullfile(script_folder, '..', 'objective_functions.m'));
 l_values = 0.01:0.005:0.1;
 
 figure;
-sgtitle('Golden Section: Function evaluations vs l', 'FontWeight','bold');
+sgtitle('Dichotomous (with derivative): Function evaluations vs l', 'FontWeight','bold');
 for i = 1:3
     fevals_l = zeros(size(l_values));
     for j = 1:length(l_values)
         l = l_values(j);
-        [~,~,~,fevals,~] = golden_section(funcs{i}, a0, b0, l);
+        [~,~,~,fevals,~] = dichotomous_derivative(dfuncs{i}, a0, b0, l);
         fevals_l(j) = fevals;
     end
     subplot(3,1,i);
@@ -28,17 +28,17 @@ l_values = [0.1, 0.01, 0.003];
 colors = lines(length(l_values));
 
 figure;
-sgtitle('Golden Section: Interval endpoints a_k, b_k vs iteration k for different l', 'FontWeight','bold');
+sgtitle('Dichotomous (with derivative): Interval endpoints a_k, b_k vs iteration k for different l', 'FontWeight','bold');
 for i = 1:3
     for j = 1:length(l_values)
         l = l_values(j);
-        [~, ~, k, ~, history] = golden_section(funcs{i}, a0, b0, l);
+        [~, ~, k, ~, history] = dichotomous_derivative(dfuncs{i}, a0, b0, l);
 
         subplot(3,3,(i-1)*3 + j); hold on;
         plot(0:k, history.a, '--', 'Color', colors(j,:), 'Marker', 'o', 'LineWidth',1.2, 'DisplayName', 'a_k');
         plot(0:k, history.b, '-',  'Color', colors(j,:), 'Marker', 'o', 'LineWidth',1.2, 'DisplayName', 'b_k');
         xlabel('Iteration k'); ylabel('a_k, b_k');
-        title([fnames{i}, ', l=', num2str(l)], 'FontWeight','bold', 'FontSize', 12);
+        title([fnames{i}, ', l=', num2str(l)], 'FontWeight','bold','FontSize',12);
         grid on;
         legend('show');
     end
