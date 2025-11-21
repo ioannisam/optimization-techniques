5582e4625865becc7c8a611f5ecdc80f9fd03a4d
ioannis <i.michalainas@gmail.com>
2025-11-22 00:55:06 +0200

    complete task2

--- assignment2/objective_functions.m
+++ assignment2/objective_functions.m
@@ -8,8 +8,19 @@ f  = matlabFunction(f_sym,  'Vars', {x, y});
 gf = matlabFunction(gf_sym, 'Vars', {x, y});
 hf = matlabFunction(hf_sym, 'Vars', {x, y});
 
+f_wr = @(v) f(v(1), v(2));
+gf_wr = @(v) reshape(gf(v(1), v(2)), [], 1);
+hf_wr = @(v) hf(v(1), v(2));
+
 initial_points = {
     [ 0,  0];
     [-1, -1];
     [ 1,  1];
 };
+npoints = numel(initial_points);
+
+methods = {'fixed','optimal','armijo'};
+nmethods = numel(methods);
+
+gamma_fixed = 0.1;
+e = 1e-6;
