44327aa68825d71542848146f441015d6eca6203
ioannis <i.michalainas@gmail.com>
2025-12-05 11:49:18 +0200

    add objective_functions.m

---
+++ assignment3/objective_functions.m
@@ -0,0 +1,21 @@
+syms x1 x2
+
+f_sym  = (1/3)*x1^2 + 3*x2^2;
+gf_sym = gradient(f_sym, [x1, x2]);
+
+f  = matlabFunction(f_sym,  'Vars', {x1, x2});
+gf = matlabFunction(gf_sym, 'Vars', {x1, x2});
+
+f_wr  = @(v) f(v(1), v(2));
+gf_wr = @(v) reshape(gf(v(1), v(2)), [], 1);
+
+P = @(v) [min(max(v(1), -10), 5); min(max(v(2), -8), 12)];
+
+initial_points = {
+    [5,  -5];
+    [-5, 10];
+    [8, -10];
+};
+npoints = numel(initial_points);
+
+e = 1e-6;
